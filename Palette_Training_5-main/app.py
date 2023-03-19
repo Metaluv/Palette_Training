5cf0ebdcc496be6bd5436bd1a9d0c16bebb51dff
Jason Cardinal <93737375+Metaluv@users.noreply.github.com>
2023-03-19 00:49:41 -0600

    update

--- Palette_Training_5-main/app.py
+++ Palette_Training_5-main/app.py
@@ -10,7 +10,7 @@ import warnings
 
 warnings.simplefilter(action='ignore', category=ValueWarning)
 
-DATA_PATH = "data/rm-yields-data.csv"
+DATA_PATH = "https://raw.githubusercontent.com/Metaluv/Palette_Training/main/Palette_Training_5-main/data/rm-yields-data.csv"
 
 def load_data():
     return pd.read_csv(DATA_PATH)
