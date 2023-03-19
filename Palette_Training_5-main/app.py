7808db64838beec8518cae40d71a758a09571ecc
Jason Cardinal <93737375+Metaluv@users.noreply.github.com>
2023-03-19 14:33:14 -0600

    update

--- Palette_Training_5-main/app.py
+++ Palette_Training_5-main/app.py
@@ -11,7 +11,18 @@ import warnings
 
 warnings.simplefilter(action='ignore', category=ValueWarning)
 
+# Read in geo data
+geo_df = gpd.read_file('Rural Municipality\Rural Municipality.shp')
+
 DATA_PATH = "https://raw.githubusercontent.com/Metaluv/Palette_Training/main/Palette_Training_5-main/data/rm-yields-data.csv"
+
+# rename the column in the geo_df
+geo_df.rename(columns={'RMNO': 'RM',}, inplace=True)
+
+# change the data type of the RM column in the geo_df to int64
+geo_df['RM'] = geo_df['RM'].astype('int64')
+
+merged = geo_df.merge(DATA_PATH, on='RM', how='left')
 
 def load_data():
     return pd.read_csv(DATA_PATH)
